52adc6e42aebe731fc6b7cf85bf9fdde656552d0
Manda Ramaroherimbola <manda.ramaroherimbola@raketa.mg>
2025-09-29 21:57:01 +0300

    UsePostData.tsx - add task

--- src/component/Hooks/UsePostData.tsx
+++ src/component/Hooks/UsePostData.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import {useRecoilState, useSetRecoilState} from "recoil";
 import {TaskState, TaskType} from "../Application/State/TaskState";
 import {loaderImage} from "../Application/State/BackgroundImage";
+import {addDoc, collection, serverTimestamp} from "firebase/firestore";
+import {db} from "../../firebase";
 
 const usePostData = (url: string, update: any) => {
 
     const [dataTask, setDataTask]:[Array<TaskType>,any] = useRecoilState(TaskState)
     const setProgress = useSetRecoilState(loaderImage)
-    const fetchData = async ( data: any ) => {
-        const response: any = await fetch( url , {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ ...data })
-        })
 
-        return await response.json()
-    }
+    const saveData = async (data: any, collectionName: string) => {
+        try {
+            const docRef = await addDoc(collection(db, collectionName), {
+                ...data,
+                createdAt: serverTimestamp(), // optionnel
+            });
+
+            return { id: docRef.id, ...data }; // tu récupères l'ID + tes données
+        } catch (e) {
+            console.error("❌ Erreur Firestore:", e);
+            throw e;
+        }
+    };
 
     return async (ev: SubmitEvent) => {
         ev.preventDefault()
@@ -23,12 +30,14 @@ const usePostData = (url: string, update: any) => {
         const form = ev.target as HTMLFormElement
         const formData = new FormData( form )
         const newData = Object.fromEntries( formData.entries() )
+        const pathName: any = url.split("/").at(-2)
 
         try {
-            const res = await fetchData({ ...newData, ...update })
-            setDataTask([ ...dataTask, res.newData ])
-            setProgress( res.newData === undefined )
-
+            const res = await saveData({ ...newData, ...update }, pathName );
+            // affichier une tache
+            setDataTask([...dataTask, res]);
+            // Une Progresse
+            setProgress( res === undefined )
             form.reset()
         } catch (e) {
             console.log(e)
