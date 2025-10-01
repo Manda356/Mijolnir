b0299730e0a39082a710c485fa037351f9f488e0
Manda Ramaroherimbola <manda.ramaroherimbola@raketa.mg>
2025-10-01 15:59:29 +0300

    CheckDone.tsx - done or not

--- src/component/Application/Button/Check_Done/CheckDone.tsx
+++ src/component/Application/Button/Check_Done/CheckDone.tsx
@@ -3,33 +3,33 @@ import IconButton from "@mui/material/IconButton";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import {Tooltip} from "@mui/material";
-import {useRecoilState, useRecoilValue} from "recoil";
+import {useRecoilState,} from "recoil";
 import {TaskOpen, TaskState, TaskType} from "../../State/TaskState";
 import PicAudio from "../../../../assets/Audio/Pic.mp3";
+import {doc, updateDoc} from "firebase/firestore";
+import {db} from "../../../../firebase";
 
 const CheckDone = ( props: any ) => {
-    const url = 'http://localhost:5000/tasks/'
     const [tasksData, setTasksData] = useRecoilState(TaskState)
     const CheckDone = async () => {
 
         if( props.reference === "task" ){
             const songs = new Audio( PicAudio )
-            const newData: any = tasksData.map(( item: TaskType ) => item._id === props.task._id ? { ...props.task, done: !props.check } : item)
+            const newData: any = tasksData.map(( item: TaskType ) => item.id === props.task._id ? { ...props.task, done: !props.check } : item)
 
             try {
+                const taskRef = doc(db, "tasks", props.task._id);
 
-                await fetch( url + props.task._id,{
-                    method: "PUT",
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({ ...props.task, done: !props.check })
-                })
+                await updateDoc(taskRef, {
+                    ...props.task,
+                    done: !props.check
+                });
 
                 props.setCheck(!props.check)
 
                 !props.check ? songs.play() : songs.pause()
 
                 setTasksData( newData )
-
             }catch (e) {
                 console.log(e)
             }
