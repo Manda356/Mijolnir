42498339fff06274deef4cdcf18fb19b6748a2be
Manda Ramaroherimbola <manda.ramaroherimbola@raketa.mg>
2025-10-02 16:45:44 +0300

    [FEATURE] - Sign_in.tsx new tab for open link

--- src/component/Authentification/Copyright.tsx
+++ src/component/Authentification/Copyright.tsx
@@ -5,7 +5,10 @@ const Copyright = (props: any) => {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://manda-portfolio.webflow.io/">Manda Herimbola</Link>{' '}
+            <Link color="inherit"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://manda-portfolio.webflow.io/">Manda Herimbola</Link>{' '}
             {new Date().getFullYear()}
             {'.'}
         </Typography>
